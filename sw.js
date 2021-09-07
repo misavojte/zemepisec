4b904ffbf3cf94dfac51541c9688fd27a525895a
Michaela Vojtěchovská <mich.vojtechovska@gmail.com>
2021-09-07 19:09:19 +0200

    Drobné pokusy

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ca66c75c8a3eabfdf9e9.js"
+    "url": "webpack-runtime-f94a0b8796d86437cb5e.js"
   },
   {
-    "url": "framework-aef765d93369ce7783c1.js"
+    "url": "framework-f9aa32220b0b280ffb3d.js"
   },
   {
-    "url": "app-8aeabec7876ebb94d198.js"
+    "url": "app-3b3850b887b9136a22e0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8ccf9c900cf584fe9e2144bf0834c3b2"
+    "revision": "d6dce0e3bb69374cebdad6d535a14c46"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,18 +48,18 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "90fca9d8a4f16015ef635e8ccf431831"
+    "revision": "8ef858b8bb6d21d2d282a54204594763"
   },
   {
-    "url": "polyfill-5bb25cd6ceabf6441f0f.js"
+    "url": "polyfill-9428f51528a38aba061d.js"
   },
   {
     "url": "manifest.json",
-    "revision": "ddc4e6299134fc57bfb6dd3138699621"
+    "revision": "b3abc8da35003e107fc90347af60423e"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "d51e2e827c363147e64e6440eb700cce"
+    "revision": "cb775c3e55abd6c74d7d46a5f5643c25"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8aeabec7876ebb94d198.js`))) {
+  if (!resources || !(await caches.match(`/app-3b3850b887b9136a22e0.js`))) {
     return await fetch(event.request)
   }
 
